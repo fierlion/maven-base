cba6645c41cacd8f25a997a3600d4df00ccff85a
fierlion <fierlion@gmail.com>
2016-09-27 19:23:45 -0700

    added readme and updated version

---
+++ src/main/java/com/fierlion/getargs/ArrayMult.java
@@ -0,0 +1,12 @@
+package com.fierlion.getargs;
+
+import java.util.List;
+import java.lang.Integer;
+
+public class ArrayMult {
+    public static Integer multiplyArrays(List<Integer> first, List<Integer> second) {
+        System.out.println(first);
+        System.out.println(second);
+        return 5;
+    }
+}
--- src/main/java/com/fierlion/getargs/Main.java
+++ src/main/java/com/fierlion/getargs/Main.java
@@ -1,9 +1,18 @@
 package com.fierlion.getargs;
 
+import java.util.List;
+import java.lang.Integer;
+import java.util.ArrayList;
+
 public class Main {
     public static void main (String[] args) {
-        Decpal.isPalindrome(101, 3);
-        //new Cli(args).parse();
+        List<Integer> first = new ArrayList<>();
+        List<Integer> second = new ArrayList<>();
+        first.add(5);
+        second.add(4);
+        second.add(1);
+        ArrayMult ar = new ArrayMult();
+        ar.multiplyArrays(first,second);
     }
 }
 
