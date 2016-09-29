b3df971070736030ba2750345475bd2d6b738678
fierlion <fierlion@gmail.com>
2016-09-28 20:06:48 -0700

    updated with reverse iteration

--- src/main/java/com/fierlion/getargs/ArrayMult.java
+++ src/main/java/com/fierlion/getargs/ArrayMult.java
@@ -2,11 +2,21 @@ package com.fierlion.getargs;
 
 import java.util.List;
 import java.lang.Integer;
+import java.util.ArrayList;
+import java.util.ListIterator;
 
 public class ArrayMult {
     public static Integer multiplyArrays(List<Integer> first, List<Integer> second) {
-        System.out.println(first);
-        System.out.println(second);
-        return 5;
+        //what happens with negative?
+        Integer result = 0;
+        //is it possible to use a primitive list? 
+        //length n + m n=first.length, m=second.length
+        List<Integer> results = new ArrayList<>();
+        for (int i = first.size()-1; i >= 0; i--) {
+            for (int j = second.size()-1; j >= 0; j--) {
+                System.out.println(String.format("%d, %d", first.get(i), second.get(j)));
+            }
+        }
+        return result;
     }
 }
--- src/main/java/com/fierlion/getargs/Main.java
+++ src/main/java/com/fierlion/getargs/Main.java
@@ -3,14 +3,16 @@ package com.fierlion.getargs;
 import java.util.List;
 import java.lang.Integer;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     public static void main (String[] args) {
         List<Integer> first = new ArrayList<>();
         List<Integer> second = new ArrayList<>();
-        first.add(5);
-        second.add(4);
-        second.add(1);
+        Integer[] firstNum = {1,2,3};
+        Integer[] secondNum = {1, 0};
+        first.addAll(Arrays.asList(firstNum));
+        second.addAll(Arrays.asList(secondNum));
         ArrayMult ar = new ArrayMult();
         ar.multiplyArrays(first,second);
     }
